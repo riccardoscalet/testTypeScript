cd5c7e7a772c69831bfe3b3e9dd16f4ecf4c4d12
riccardoscalet <rscalet@techimp.com>
2016-09-29 16:01:42 +0200

    Fixed TypeScript transpilation.

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+var BananaSuper = (function () {
+    function BananaSuper() {
+    }
+    BananaSuper.prototype.banana = function (s) {
+        return "Banana" + " " + s;
+    };
+    return BananaSuper;
+}());
+exports.BananaSuper = BananaSuper;
+//# sourceMappingURL=index.js.map
